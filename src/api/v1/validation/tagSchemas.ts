6a043cd46db3863657007babbe5a75d88053406f
= <speckq4@outlook.com>
2025-11-11 23:14:23 -0600

    feat: add tag validation schemas

---
+++ src/api/v1/validation/tagSchemas.ts
@@ -0,0 +1,62 @@
+import Joi from "joi";
+
+export const tagSchemas = {
+    create: {
+        body: Joi.object({
+            name: Joi.string().required().messages({
+                "any.required": "Name is required",
+                "string.empty": "Name cannot be empty",
+            }),
+            description: Joi.string().optional().messages({
+                "string.empty": "Description cannot be empty",
+            }),
+            // to be removed once authentication is implemented
+            userId: Joi.string().required().messages({
+                "any.required": "User ID is required",
+                "string.empty": "User ID cannot be empty",
+            }),
+        }).required().messages({
+            "any.required": "Request Body is required",
+        }),
+    },
+    getById: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "ID is required",
+                "string.empty": "ID cannot be empty",
+            })
+        }).required().messages({
+            "any.required": "Request Parameters are required",
+        }),
+    },
+    delete: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "ID is required",
+                "string.empty": "ID cannot be empty",
+            })
+        }).required().messages({
+            "any.required": "Request Parameters are required",
+        }),
+    },
+    update: {
+        params: Joi.object({
+            id: Joi.string().required().messages({
+                "any.required": "ID is required",
+                "string.empty": "ID cannot be empty",
+            })
+        }).required().messages({
+            "any.required": "Request Parameters are required",
+        }),
+        body: Joi.object({
+            name: Joi.string().optional().messages({
+                "string.empty": "Name cannot be empty",
+            }),
+            description: Joi.string().optional().messages({
+                "string.empty": "Description cannot be empty",
+            }),
+        }).required().messages({
+            "any.required": "Request Body is required",
+        }),
+    },
+};
