525e038e38e8bec3dc1d6fcb066f15924a05cd9f
Xander <aburnett2@academic.rrc.ca>
2025-11-16 18:20:46 -0600

    document: word validation schemas

--- src/api/v1/validation/wordSchemas.ts
+++ src/api/v1/validation/wordSchemas.ts
@@ -1,5 +1,60 @@
 import Joi from "joi";
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Word:
+ *       type: object
+ *       required
+ *         - id
+ *         - name
+ *         - userId
+ *         - createdAt
+ *         - updatedAt
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the word
+ *           example: "531HFDGS5237976"
+ *         name:
+ *           type: string
+ *           description: The name of the word
+ *           example: "Rain World"
+ *         description:
+ *           type: string
+ *           description: A description of the word
+ *           example: "A difficult 2D platformer game."
+ *         tagIds:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: The IDs of the tags for this word
+ *           example: ["TAG1415", "TAG7423", "TAG7542"]
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: When this word was created
+ *           example: "2024-01-20T14:45:00Z"
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: When this word was last updated
+ *           example: "2024-01-20T14:45:00Z"
+ *         userId:
+ *           type: string
+ *           description: The ID of the user which created this word
+ *           example: "USER531736"
+ *         historicalId:
+ *           type: string
+ *           description: The ID of the previous version of this word (in the case this word is pending changes or publication)
+ *           example: "WORD622624"
+ *         visibility:
+ *           type: string
+ *           enum: [private, public, pending_publication, pending_deletion]
+ *           description: Describes who is allowed to view this word
+ *           example: "private"
+ */
 export const wordSchemas = {
     create: {
         body: Joi.object({
